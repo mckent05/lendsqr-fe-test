66bdc81caa7396ad5b4bf1cd180f47559b14d60e
mckent05 <atemitope95@gmail.com>
2024-07-24 18:15:25 +0100

    Added skeletal wireframes for all pages and configured routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/Login/LoginPage';
+import Main from './pages/Main';
+import Dashboard from './pages/Dashboard/Dashboard';
+import UserDetails from './pages/UserDetails/UserDetails';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<LoginPage />} />
+        <Route path='/dashboard/' element= {<Main />}>
+          <Route index element={<Dashboard />} />
+          <Route index path='users' element={<Dashboard />} />
+          <Route index path='/dashboard/users/:id' element={<UserDetails />} />
+        </Route>
       </Routes>
     </div>
   );
---
+++ src/pages/UserDetails/UserDetails.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserDetails = () => {
+  return (
+    <div>
+      <h1>user details page</h1>
+    </div>
+  )
+}
+
+export default UserDetails
